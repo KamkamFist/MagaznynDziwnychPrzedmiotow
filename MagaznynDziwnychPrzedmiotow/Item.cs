af4bf040579ed4624f2c3564c0b2618d389214c3
adamd <adamd@DESKTOP-BENDQ9F>
2025-04-22 11:21:59 +0200

    Dodaj pliki projektów.

---
+++ MagaznynDziwnychPrzedmiotow/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Item
+{
+    public string Name { get; set; }
+    public double WeightKg { get; set; }
+    public int WeirdnessLevel { get; set; }
+    public bool IsFragile { get; set; }
+
+    public Item(string name, double weightKg, int weirdnessLevel, bool isFragile)
+    {
+        Name = name;
+        WeightKg = Math.Round(weightKg, 3);
+        WeirdnessLevel = weirdnessLevel;
+        IsFragile = isFragile;
+    }
+
+  
+}
---
+++ MagaznynDziwnychPrzedmiotow/Storage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Storage
+{
+    public int Capacity { get; private set; }
+    public int CurrentItemCount { get; private set; } = 0;
+    public double MaxTotalWeight { get; private set; }
+    private double currentWeight = 0.0;
+
+    private List<Item> items = new List<Item>();
+
+    public Storage(int capacity, double maxTotalWeight)
+    {
+        Capacity = capacity;
+        MaxTotalWeight = Math.Round(maxTotalWeight, 3);
+    }
+
+
+}
